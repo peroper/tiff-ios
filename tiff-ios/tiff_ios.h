1c00e132284e2d86783c9451214662b48c8bd281
Albin Törnqvist <albin.tornqvist@2xper.se>
2021-09-30 10:43:30 +0200

    Fixed the framework header.

--- tiff-ios/tiff_ios.h
+++ tiff-ios/tiff_ios.h
@@ -6,8 +6,23 @@
 //  Copyright (c) 2017 NGA. All rights reserved.
 //
 
-#ifndef tiff_ios_tiff_ios_h
-#define tiff_ios_tiff_ios_h
-
-
-#endif
+#import <tiff_ios/TIFFFieldTagTypes.h>
+#import <tiff_ios/TIFFFieldTypes.h>
+#import <tiff_ios/TIFFFileDirectory.h>
+#import <tiff_ios/TIFFFileDirectoryEntry.h>
+#import <tiff_ios/TIFFImage.h>
+#import <tiff_ios/TIFFImageWindow.h>
+#import <tiff_ios/TIFFRasters.h>
+#import <tiff_ios/TIFFReader.h>
+#import <tiff_ios/TIFFWriter.h>
+#import <tiff_ios/TIFFCompressionDecoder.h>
+#import <tiff_ios/TIFFCompressionEncoder.h>
+#import <tiff_ios/TIFFDeflateCompression.h>
+#import <tiff_ios/TIFFLZWCompression.h>
+#import <tiff_ios/TIFFPackbitsCompression.h>
+#import <tiff_ios/TIFFRawCompression.h>
+#import <tiff_ios/TIFFUnsupportedCompression.h>
+#import <tiff_ios/TIFFByteReader.h>
+#import <tiff_ios/TIFFByteWriter.h>
+#import <tiff_ios/TIFFIOUtils.h>
+#import <tiff_ios/TIFFConstants.h>
